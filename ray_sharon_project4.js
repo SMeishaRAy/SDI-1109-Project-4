70678df61988de902ef3f0178c00deb34d8dd264
S. Meisha Ray <bluedragoness@fullsail.edu>
2011-09-19 12:43:36 -0400

    Finally got my array to sort and reverse.

--- ray_sharon_project4.js
+++ ray_sharon_project4.js
@@ -6,8 +6,7 @@
 STRING
 1. Does a string follow a 123-456-7890 pattern like a phone number?
 */
-		var myString = 123-456-7890;
-		console.log(myString.substring(0,10));
+		
 /*
 NUMBER
 2. Format a number to use a specific number for decimal places as for money: 2.1 --> 2.10. 
@@ -36,34 +35,38 @@ ARRAY
 /*STRING
 7. Is the string a URL? (Does it start with http: or https:?)
 */
+	
 
 /*NUMBER
 8. Find the number of hours or days difference between two dates.
 */
 	//How many days from the day you were born?
-		var date1 = new Date(2011, 8, 16); //new Date
+		var date1 = new Date(2011, 8, 19); //new Date
         var date2 = new Date(1979, 2, 10);
         var msDif = date1.getTime() - date2.getTime(); // difference in milliseconds
         var msDay = 1000 * 60 * 60 * 24 // milliseconds in one day
         var differentDays = Math.ceil(msDif / msDay);  // difference in days
-        	console.log("The difference between September 16, 2011 and the day I was born is " + differentDays + " days.");	
+        	console.log("The difference between September 19, 2011 and the day I was born is " + differentDays + " days.");	
 		 		 	
 /*ARRAY
 9. Given an array of objects and the name of a key, return the array sorted by the value of that key in each of the objects:
 	"a" + [{a:2}, {a:3}, {a:1}]
 	--> [{a:1}, {a:2}, {a:3}].
-(array.reverse)
-*/
-							//CANNOT GET THIS ONE//
-			var aNum = [{a:2}, {a:3}, {a:1}];
-			for(key in aNum){
-			console.log(aNum);
-			}
-			
+(array.reverse) & (array.sort)
+
+			//Prints out a:1, a:3, a:2 in order of a:1, a:2, a:3, or a:3, a:2, a:1*/
+			var aNum = ["a:2", "a:3", "a:1"];
+			console.log(aNum.sort());
+			console.log(aNum.reverse());	
 			
 /*STRING
 10. Title-case a string (split into words, then uppercase the first letter of each word).
 */
+//Style-String//
+
+	var word = "javascript rules!";
+		console.log(word.toLowerCase());
+	console.log(word.toUpperCase());
 
 /*NUMBER
 11. Given a string version of a number such as "42", return the values as an actual Number, such as 42.
