c8452827bdd007aceff74385b0e28fce070713c9
S. Meisha Ray <bluedragoness@fullsail.edu>
2011-09-20 17:00:21 -0400

    Adjusted comments and finished my last function.

--- ray_sharon_project4.js
+++ ray_sharon_project4.js
@@ -1,59 +1,46 @@
-//Sharon "Meisha" Ray
+//Author :Sharon "Meisha" Ray
 //SDI 1109 Project 4
-//Code Library!
+//September 22, 2011
+//My Code Library!
 
 /*STRING
 1. Does a string follow a 123-456-7890 pattern like a phone number?
 */
 		function checkNum(phone){
-		var number = /^(\d{3})[-](\d{3})[-](\d{4})$/;
+		var number = /^(\d{3})[-](\d{3})[-](\d{4})$/; // verifies the format matches.
 			if(!phone.match(number)){
-				console.log("Your number is not valid.");
+				console.log("Your phone number is not valid."); //if it doesn't match, it will print false.
 				return false;
 				}else { 
 					if (phone.match(number)) {
-					console.log("Your phone number is valid.");
+					console.log("Your phone number is valid.");//if it does match, it prints true.
 				return true;
 				}
 				}
 				};
-				checkNum("123-956-7890");
-				
-/*NUMBER
-2. Format a number to use a specific number for decimal places as for money: 2.1 --> 2.10. 
-		I tried both syntaxes here (.toFixed & .toPrecision).
-*/
-	var money = 2.1;
-	console.log(money.toFixed(2)); //.toFixed - moves the decimal (2) places.
-	
-	/*var money = 2.1
-	console.log(money.toPrecision(3));*/ //.toPrecision
-	
-/*ARRAY
-3. Find the smallest value in an array that is greater than a given number.
-*/
-		
-
-/*STRING
-4. Does a string follow an aaa@bbb.ccc pattern like an email address?
-*/
-
-/*NUMBER
-5. Fuzzy-match a number: is the number above or below a number within a certain percent?
-*/
-
-/*ARRAY
-6. Find the total value of just the numbers in an array, even if some of the items are not numbers.
-*/
+				checkNum("123-456-7890");
 			
-	
 /*STRING
-7. Is the string a URL? (Does it start with http: or https:?)
+2. Is the string a URL? (Does it start with http: or https:?)
 */
+		function checkUrl(url){
+		var sites = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/ 
+		// verifies the if the string is a http: or https:
+			if(!url.match(sites)){
+				console.log("Your url is not valid."); //if it does not have http: or https: in the url, it will print false.
+				return false;
+				}else { 
+					if (url.match(sites)) {
+					console.log("Your url is valid.");//if it does have http: or https: in the url, it prints true.
+				return true;
+				}
+				}
+				};
+				
+				checkUrl("http:", "https:");	
 	
-
 /*NUMBER
-8. Find the number of hours or days difference between two dates.
+3. Find the number of hours or days difference between two dates.
 */
 	//How many days from the day you were born?
 		var date1 = new Date(2011, 8, 19); //new Date
@@ -64,7 +51,7 @@
         	console.log("The difference between September 19, 2011 and the day I was born is " + differentDays + " days.");	
 		 		 	
 /*ARRAY
-9. Given an array of objects and the name of a key, return the array sorted by the value of that key in each of the objects:
+4. Given an array of objects and the name of a key, return the array sorted by the value of that key in each of the objects:
 	"a" + [{a:2}, {a:3}, {a:1}]
 	--> [{a:1}, {a:2}, {a:3}].
 	(array.sort)*/
@@ -80,7 +67,7 @@
 			console.log(aNum);
 						
 /*STRING
-10. Title-case a string (split into words, then uppercase the first letter of each word).
+5. Title-case a string (split into words, then uppercase the first letter of each word).
 */
 //Style-String//
 
@@ -92,7 +79,7 @@
 	console.log(words); //Displays "Title Case Full Sail Rules!"
 	
 /*NUMBER
-11. Given a string version of a number such as "42", return the values as an actual Number, such as 42.
+6. Given a string version of a number such as "42", return the values as an actual Number, such as 42.
 */
 	var myNum = "42",
 		number = function(){
@@ -101,14 +88,6 @@
 		}
 	console.log("My number is " + number());
 	
-	/*This one is another way of getting the same number
-	var myNum = "42";
-	console.log("My Number is " + myNum.valueOf());*/
-
-/*STRING
-12. Given a string that is a list of things separated by a given string, as well as another string separator changed to the second: "a,b,c" + "," + "/" --> "a/b/c".
-*/
 	
-
 
 
